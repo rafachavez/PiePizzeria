6059ae86caccf2ea9d0f34ec74824c83166bb09a
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-05-17 20:36:44 -0600

    Add project files.

---
+++ PiePizzeria/Models/iPieRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PiePizzeria.Models
+{
+    public interface iPieRepository
+    {
+        IEnumerable<Pie> GetAllPies();
+        Pie GetPieById(int pieId);
+    }
+}
