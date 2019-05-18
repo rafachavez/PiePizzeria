894dc671ae177673cfc7b13443ccb02f3e1f97c9
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-05-18 02:34:23 -0600

    Added dependency injection on the homeController

--- PiePizzeria/Controllers/HomeController.cs
+++ PiePizzeria/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PiePizzeria.Models;
 
 namespace PiePizzeria.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly iPieRepository _pieRepository;
+
+        public HomeController(iPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
         public ViewResult Index()
         {
             ViewBag.Message = "Welcome to the Pie Pizzeria!";
